a91f43cf8bfdd5363e15722442ba96622d71a529
Joe Kozlowski <kozineffect@github.com>
2017-05-01 09:07:19 -0400

    constructed basic back end implementation and included driver file

---
+++ ClozeCard.js
@@ -0,0 +1,10 @@
+var ClozeCard = function(text, cloze){
+    this.text = text;
+    this.cloze = cloze;
+    this.partial = text.replace(cloze, ". . . ");
+    var check = text.includes(cloze);
+    if (check !== true){
+        console.log("There was an error in your input!");
+    }
+};
+module.exports = ClozeCard;
